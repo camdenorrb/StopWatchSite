7f8bf69dddb3029e61b2fde4cec388298d08c01e
camdenorrb <camdenorrb@users.noreply.github.com>
2017-08-11 01:41:59 -0500

    Meow

--- assets/js/homepage/ButtonThingy.js
+++ assets/js/homepage/ButtonThingy.js
@@ -4,6 +4,7 @@ function thing(id) { id.innerHTML = "Meow" }
 
 const image = new Image(200, 200)
 $.getJSON("https://random.cat/meow", function(data) {
+  image.id = "Meowth"
   image.src = data.file.replace("http", "https")
   document.body.appendChild(image)
 })
