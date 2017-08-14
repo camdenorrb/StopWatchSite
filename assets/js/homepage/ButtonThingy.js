6dd829354252917c4343a3be027a7b8254bb9d7e
camdenorrb <camdenorrb@users.noreply.github.com>
2017-08-13 23:45:15 -0500

    Very basic stop watch

--- assets/js/homepage/ButtonThingy.js
+++ assets/js/homepage/ButtonThingy.js
@@ -1,10 +1,38 @@
 //document.getElementById("button").addEventListener("click", myFunction);
 
-function thing(id) { id.innerHTML = "Meow" }
+//const image = new Image(200, 200)
 
-const image = new Image(200, 200)
-$.getJSON("https://random.cat/meow", function(data) {
+var interval
+
+var seconds = 1
+
+const element = document.getElementById("text")
+
+function start() {
+  if (interval != null) return
+  interval = setInterval(tick, 1000)
+}
+
+function reset() {
+  if (seconds == 1) return
+  stop()
+  seconds = 1
+  element.innerHTML = 0
+}
+
+function stop() {
+  if (interval == undefined) return
+  clearInterval(interval)
+  interval = undefined
+}
+
+function tick() {
+  element.innerHTML = seconds++
+}
+
+
+/*$.getJSON("https://random.cat/meow", function(data) {
   image.id = "Meowth"
   image.src = data.file.replace("http", "https")
   document.body.appendChild(image)
-})
+})*/
